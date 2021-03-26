c91ca508f7fb2c01cf2894905bb9000e3a6824ab
KevinKaras <kevinkaras2@gmail.com>
2021-03-26 18:49:44 -0400

    user log in log out routes finalized

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -3,6 +3,13 @@ const asyncHandler = require('express-async-handler');
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
 
 router.post('/test', function (req, res) {
     res.json({requestBody: req.body});
