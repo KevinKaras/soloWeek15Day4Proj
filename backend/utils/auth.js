a972e9dd60cfdda074bc9a4720bfe58a4024f329
KevinKaras <kevinkaras2@gmail.com>
2021-03-25 23:05:49 -0400

    tested requireauth working properly

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -64,18 +64,7 @@ const requireAuth = [
     },
 ];
 
-const requireAuth = [
-    restoreUser,
-    function (req, res, next) {
-      if (req.user) return next();
-  
-      const err = new Error('Unauthorized');
-      err.title = 'Unauthorized';
-      err.errors = ['Unauthorized'];
-      err.status = 401;
-      return next(err);
-    },
-];
+
 
 module.exports = { setTokenCookie, restoreUser, requireAuth };
 
